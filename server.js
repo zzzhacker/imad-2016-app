2db0d09e06dac5a17b512e0294b29cd16e95a6c4
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-19 09:17:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.send('this is article two response');
+        res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send('this is article three response');
+        res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
