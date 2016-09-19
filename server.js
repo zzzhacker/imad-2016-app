e43b94b67cc09403d0c4b1570a761f3dca55296b
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-19 09:05:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('this is article one response');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
