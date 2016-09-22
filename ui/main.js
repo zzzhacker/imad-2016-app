163e056cd5ca6d821468786bd0ab97f2ec386b27
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-22 12:09:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img=document.getElementById("madi");
 var marginleft=0;
 function moveRight(){
     marginleft=marginleft+1;
-    log.style.marginleft=marginleft+"px";
+    img.style.marginleft=marginleft+"px";
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
