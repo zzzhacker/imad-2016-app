e5017cece101b1b39cad4a050ee9ea2d1a8b4cfe
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-22 12:15:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
 var img=document.getElementById("madi");
-var marginleft=0;
+var marginLeft=0;
 function moveRight(){
-    marginleft=marginleft+1;
-    img.style.marginleft=marginleft+"px";
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
