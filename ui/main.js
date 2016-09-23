d44134dd0f55af697927af6f4ddbac2764f64fd8
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-23 17:51:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button=document.getElementBuId('counter');
 var counter=0;
 button.onclick=function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function () {
+        if(request.readystate ===XMLHttpRequest.Done){
+            if(request.status==200){
+                var counter=request.responseText();
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    request.open('GET','http://zzzhacker.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
