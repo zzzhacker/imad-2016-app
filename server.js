819c1fc70b47f63030f7df162df5b9ddf8b44901
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-26 19:35:42 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var counter=0;
-app.ger('/counter',function(req,res){
+app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
