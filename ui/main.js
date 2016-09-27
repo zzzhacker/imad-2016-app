0ce04e35ed92f86139332e817f4a8ffeb9016096
zzzhacker <yogesh.grjr@rediffmail.com>
2016-09-26 20:14:32 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ button.onclick=function(){
     request.onreadystatechange=function () {
         if(request.readystate ===XMLHttpRequest.Done){
             if(request.status===200){
-                var counter=request.responseText();
+                var counter=request.responseText;
                 var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
             }
