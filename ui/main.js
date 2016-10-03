de44cd3abd3f3a3a74021b0ec6e90479e8efa615
zzzhacker <yogesh.grjr@rediffmail.com>
2016-10-03 14:25:07 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var names={name1,name2,name3};
     var list='';
-    for(var i=0;i<name.length;i++){
+    for(var i=0;i<names.length;i++){
         list +='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
