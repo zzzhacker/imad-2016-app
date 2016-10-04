244ecaa669ef79b791f2b9d6fe9df0b8b6b38145
zzzhacker <yogesh.grjr@rediffmail.com>
2016-10-03 23:22:41 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ var names=[];
 app.get('/submit-name',function(req,res){
     var name=req.query.name;
     names.push(name);
-    res.send(JSON.stringfy(names));
+    res.send(JSON.stringlfy(names));
 });
 
 app.get('/:articleName',function(req,res){
