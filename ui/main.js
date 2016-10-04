c34884373db5f53b3795152234a81a8dfbcfcdb1
zzzhacker <yogesh.grjr@rediffmail.com>
2016-10-03 23:55:30 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ submit.onclick=function(){
         if(request.readystate ===XMLHttpRequest.Done){
             if(request.status===200){
                 var names=request.responseText;
-                names=JSON.parse(names;)
+                names=JSON.parse(names);
                 var list='';
                 for(var i=0;i<names.length;i++){
                     list +='<li>'+names[i]+'</li>';
