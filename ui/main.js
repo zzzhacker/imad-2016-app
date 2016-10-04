f45fd608ddeefcb2d29b0a79ecb98df0fc7f235a
zzzhacker <yogesh.grjr@rediffmail.com>
2016-10-03 23:53:52 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,15 +15,29 @@ button.onclick=function(){
     request.open('GET','http://zzzhacker.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
+
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list +='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function () {
+        if(request.readystate ===XMLHttpRequest.Done){
+            if(request.status===200){
+                var names=['name1','name2','name3','name4'];
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list +='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    var nameInput=document.getElementById("name");
+    var name=nameInput.value;
+    request.open('GET','http://zzzhacker.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
 };
