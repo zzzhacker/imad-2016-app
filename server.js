4c766cac763a5f1d2cc56cc9977f381e7b837dca
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-08 23:33:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,9 +127,22 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function(req,res){
-        var articleName=req.params.articleName;
-        res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName',function(req,res){
+        pool.query("SELECT * FROM article WHERE title= "+req.params.articleName,function(err,result){
+            if(err){
+           res.status(500).send(err.toString());
+       }
+            else{
+                if(result.rows.length===0){
+                    res.status(404).send('Article not found')
+                }
+                else{
+                    var articleData=result.rows[0];
+                    res.send(createTemplate(articleData));
+                }
+            }
+        });
+        
 });
 
 
