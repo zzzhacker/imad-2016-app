af936a7d026f48fbe197e40064c8ed1714817af1
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-08 21:02:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+
+var config = {
+   host: 'db.imad.hasura.io',
+   user: 'zzzhacker',
+   password: process.env,DB_PASSWORD,
+   database: 'zzzhacker',
+   port: '5432',
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -79,9 +90,16 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-    
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           res.send(JSON.stringfy(result));
+       }
+    });
 });
 var counter=0;
 app.get('/counter',function(req,res){
