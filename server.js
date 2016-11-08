d48c2a499855a78355b4b18264774e9e2b52bb98
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-08 23:44:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/submit-name',function(req,res){
 });
 
 app.get('/article/:articleName',function(req,res){
-        pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName+"'",function(err,result){
+        pool.query("SELECT * FROM article WHERE title= $1",[req.params.articleName],function(err,result){
             if(err){
            res.status(500).send(err.toString());
        }
