fdf71005e0f9d73d062ab7da07559b64dee88693
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-08 21:05:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 
 
 var config = {
-   host: 'db.imad.hasura.io',
+   host: 'db.imad.hasura-app.io',
    user: 'zzzhacker',
    password: process.env,DB_PASSWORD,
    database: 'zzzhacker',
