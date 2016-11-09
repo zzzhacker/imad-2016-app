d1c1b386f20f35df24aa2ee03cb287871bbf205f
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-09 10:14:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/test-db',function(req,res){
 
 function hash(input,salt){
     var hashed= crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-    return hashed.toString(hex);
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input',function(req,res){
