416939607e4ab0999e29707af1eb3ab604c49ead
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-09 15:21:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/login',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
 
-    pool.query('SELECT * FROM "user" username= $1',[username],function(err,result){
+    pool.query('SELECT * FROM "user" WHERE username= $1',[username],function(err,result){
         if(err){
            res.status(500).send(err.toString());
        } 
