97de7fa3dd1b65e685dc9c260fcfb809b500f9a1
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-09 15:53:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bosyParser.json());
+app.use(bodyParser.json());
 
 function createTemplate(data){
     var title=data.title;
