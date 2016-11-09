9d10209b5955f3835a9dbb59a19643e4c10d6bed
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-09 15:49:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/create-user',function(req,res){
     var username=req.body.username;
     var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
-    var dbString=hsah(password,salt);
+    var dbString=hash(password,salt);
     pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
            res.status(500).send(err.toString());
