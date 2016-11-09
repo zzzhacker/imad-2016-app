f4a0f172a481e1ea633da462640a53351ab1f837
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-09 15:17:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser=require('body-parser')
 
 var config = {
    host: 'db.imad.hasura-app.io',
@@ -15,6 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bosyParser.json());
 
 function createTemplate(data){
     var title=data.title;
@@ -73,6 +75,49 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res){
     var hashedstring=hash(req.params.input,'this-is-random-string');
     res.send(hashedstring);
+});
+
+
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString=hsah(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           res.send('user sucessfully created : '+username)
+       }
+    });
+});
+
+
+app.post('/login',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+
+    pool.query('SELECT * FROM "user" username= $1',[username],function(err,result){
+        if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           if(result.rows.length===0){
+               res.send(403).send('usename/password is invailid');
+           }
+           else{
+               var dbString=result.rows[0].passowrd;
+               var salt=dbString.split('$')[2];
+               var hashedPassword=hash(passowrd,salt);
+               if(hashedPassword==dbString){
+                   res.send("credentials are correct");
+               }else{
+                   res.send(403).send('usename/password is invailid');
+               }
+           }
+       }
+    });
 });
 var counter=0;
 app.get('/counter',function(req,res){
