23df0dd12f5438d0d531a029aa9bbab2bb37e3f5
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-09 16:11:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser=require('body-parser');
+var session=require('express-session');
 
 var config = {
    host: 'db.imad.hasura-app.io',
