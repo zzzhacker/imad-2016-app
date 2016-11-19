2ae5a4bd63fc038a2f20d5fc2ade38113fbb2d16
zzzhacker <yogesh.grjr@rediffmail.com>
2016-11-20 00:50:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -122,7 +122,7 @@ function loadArticles () {
                 content += "</ul>"
                 articles.innerHTML = content;
             } else {
-                articles.innerHTML('Oops! Could not load all articles!');
+                articles.innerHTML('Oops! Could not load all articles!')
             }
         }
     };
